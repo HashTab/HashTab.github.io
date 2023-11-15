7b807ad06fc2180c30e38b476e1c8490367340fc
Vadim <vadzhyma@gmail.com>
2023-11-15 16:53:18 +0200

    Add "Tarla" project

---
+++ tarla/js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+  $('.slider').slick({
+    speed: 1200,
+    adaptiveHeight: true,
+    arrows: false,
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+});
